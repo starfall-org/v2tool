c2aceaa3bf07fe41e02706a888fb578f64f9bc89
gitmakes <152869680+gitmakes@users.noreply.github.com>
2023-12-18 19:18:40 +0700

    update server.py

--- server.py
+++ server.py
@@ -1,18 +1,5 @@
 import sys
 from gunicorn.app.wsgiapp import run
-from multiprocessing import Process
-from application.contentdownload import runserver
-
-def run_gunicorn():
-    sys.argv = "gunicorn --timeout 1200 --bind 0.0.0.0:3000 application.flask:app".split()
-    sys.exit(run())
-
 if __name__ == '__main__':
-    gunicorn_process = Process(target=run_gunicorn)
-    runserver_process = Process(target=runserver)
-
-    gunicorn_process.start()
-    runserver_process.start()
-
-    gunicorn_process.join()
-    runserver_process.join()
+    sys.argv = "gunicorn --timeout 1200 --bind 0.0.0.0:3000 main:app".split()
+    sys.exit(run())
