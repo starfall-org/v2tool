a7210bbf2908822bf4a1a1603787973e6b819e12
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-02 16:41:04 +0700

    update data.py

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ from deta import Deta
 deta = Deta(os.environ.get('DETA_KEY'))
 db = deta.Base("v2ray-notes")
 
-def get_all(filename):
+def get_data(filename):
   entry = db.get(filename)
   if entry:
     return entry['urls']
