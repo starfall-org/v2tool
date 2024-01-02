cb5e0d1266ff023e2ade00f4e8bb9e23ad4482da
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-02 14:10:02 +0700

    update configkit/vmess.py

--- configkit/vmess.py
+++ configkit/vmess.py
@@ -14,22 +14,22 @@ def edit(link, set_uuid=None, set_sni=None, set_tag=None):
   sni = config['sni']
   host = config['host']
   port = config['port']
-  if add in ['127.0.0.1', '1.1.1.1', '0.0.0.0', '8.8.8.8']:
+  if add in ['127.0.0.1', '1.1.1.1', '0.0.0.0', '8.8.8.8'] or {str(add):uuid} in edited:
     return
   if set_tag:
     tag = f'{name} {num}'
   if set_uuid:
     uuid = set_uuid
   if set_sni:
-    if port is 80:
+    if port == 80:
       host = set_sni
-    elif port is 443:
+    elif port == 443:
       sni = set_sni
     else:
       host = sni = set_sni
   edited.append({str()})
   num += 1
   config = json.dumps(config).encode('utf-8')
-  link = base64.b64encode(config).decode('utf-8')
-  link = f"vmess://{link}"
+  code = base64.b64encode(config).decode('utf-8')
+  link = f"vmess://{code}"
   return link
