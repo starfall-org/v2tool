5caae8f6251dfbc767c4f0e4fe73361f0174e4c1
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-02 16:24:29 +0700

    update http_req.py

---
+++ http_req.py
@@ -0,0 +1,45 @@
+import base64
+import json
+import requests
+import re
+import concurrent.futures
+import time
+import os
+
+def get_response(url):
+    response = requests.get(url, headers={"User-Agent": "v2rayNG/1.8.12"}).text
+    links = []
+    if any(proto in response for proto in ["vmess:", "trojan:", "vless:"]):
+      for link in response.splitlines():
+        if any(proto in link for proto in ["vmess:", "trojan:", "vless:"]):
+          links.append(link)
+    elif any(proto in response for proto in ["http:", "https:"]):
+      url_pattern = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+      sub_urls = re.findall(url_pattern, response)
+      links = sub_response(sub_urls)
+    else:
+      try:
+        decoded_line = base64.b64decode(response).decode('utf-8')
+        for link in decoded_line.splitlines():
+          if any(proto in link for proto in ["vmess:", "trojan:", "vless:"]):
+            links.append(link)
+      except:
+        links = []
+    return links
+    
+def sub_response(urls):
+  with concurrent.futures.ThreadPoolExecutor() as executor:
+    links = executor.map(process, urls)
+    return links
+    
+def process(url):
+  links = []
+  sub_response = requests.get(url, headers={"User-Agent": "v2rayNG/1.8.12"}).text
+  if any(proto in sub_response for proto in ["vmess:", "trojan:", "vless:"]):
+    links.extend(sub_response.splitlines())
+  else:
+    decoded_line = base64.b64decode(sub_response).decode('utf-8')
+    if any(proto in decoded_line for proto in ["vmess:", "trojan:", "vless:"]):
+      links.extend(decoded_line.splitlines())
+  return links
+    
