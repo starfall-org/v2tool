53284678cb9dac42fdf4009ce62cbf782f68e5c2
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-02 19:00:27 +0700

    update http_req.py

--- http_req.py
+++ http_req.py
@@ -31,27 +31,25 @@ def get_response(url):
     return links
     
 def get_responses(urls):
-  with concurrent.futures.ThreadPoolExecutor() as executor:
-    links = executor.map(process, urls)
-  return list(links)
-    
-def process(url):
   links = []
-  try:
-    sub_response = requests.get(url, timeout=8, headers={"User-Agent": "v2rayNG/1.8.12"})
-    if sub_response.status_code != 200:
-      return []
-    sub_response = sub_response.text
-  except:
-    sub_response = requests.get(workers, params={"url": url}, timeout=8).text
-  if any(proto in sub_response for proto in ["vmess:", "trojan:", "vless:"]):
-    links.extend(sub_response.splitlines())
-  else:
+  def process(url):
     try:
-      decoded_line = base64.b64decode(sub_response).decode('utf-8')
-      if any(proto in decoded_line for proto in ["vmess:", "trojan:", "vless:"]):
-        links.extend(decoded_line.splitlines())
+      sub_response = requests.get(url, timeout=8, headers={"User-Agent": "v2rayNG/1.8.12"})
+      if sub_response.status_code != 200:
+        return []
+      sub_response = sub_response.text
     except:
-      pass
+      sub_response = requests.get(workers, params={"url": url}, timeout=8).text
+    if any(proto in sub_response for proto in ["vmess:", "trojan:", "vless:"]):
+      links.extend(sub_response.splitlines())
+    else:
+      try:
+        decoded_line = base64.b64decode(sub_response).decode('utf-8')
+        if any(proto in decoded_line for proto in ["vmess:", "trojan:", "vless:"]):
+          links.extend(decoded_line.splitlines())
+      except:
+        pass
+  with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(process, urls)
   return links
     
