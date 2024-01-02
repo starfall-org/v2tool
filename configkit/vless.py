3aa4afd026a52f9b78f5aa1347c9b5f8fd06a9dd
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-02 22:41:26 +0700

    update configkit/vless.py

--- configkit/vless.py
+++ configkit/vless.py
@@ -4,22 +4,18 @@ def edit(link, set_uuid=None, set_sni=None, set_tag=None):
     link = link.split('://')[1]
     uuid = link.split('@')[0]
     ip, port = link.split('@')[1].split('?')[0].split(':')
-
     if port == 443:
         sni, tag = re.search(r'sni=([^&]+)&.*#(.+)', link).groups()
     else:
         sni = re.search(r'host=([^&]+)&.*#(.+)', link).group(1)
         tag = re.search(r'#(.+)', link).group(1)
-
     if ip in ['127.0.0.1', '1.1.1.1', '0.0.0.0', '8.8.8.8']:
         return
-
     if set_uuid:
         link = link.replace(uuid, set_uuid)
     if set_sni:
         link = link.replace(sni, set_sni)
     if set_tag:
         link = link.replace(tag, set_tag)
-
     full_link = f"vless://{link}"
     return full_link
