87246cd7417a6afe7d3f27a4f8aa8e25a309add4
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-02 18:01:28 +0700

    update http_req.py

--- http_req.py
+++ http_req.py
@@ -39,10 +39,12 @@ def get_responses(urls):
     
 def process(url):
   links = []
-  sub_response = requests.get(url, timeout=5, headers={"User-Agent": "v2rayNG/1.8.12"})
-  if sub_response.status_code == 200:
+  try:
+    sub_response = requests.get(url, timeout=5, headers={"User-Agent": "v2rayNG/1.8.12"})
+    if sub_response.status_code != 200:
+      return []
     sub_response = sub_response.text
-  else:
+  except:
     sub_response = requests.get(workers, params={"url": url}, timeout=5).text
   if any(proto in sub_response for proto in ["vmess:", "trojan:", "vless:"]):
     links.extend(sub_response.splitlines())
