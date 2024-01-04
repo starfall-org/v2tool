be8253b5998b821df4bc194c80b6a224b1e4d625
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-04 08:16:37 +0700

    update configkit/vless.py

--- configkit/vless.py
+++ configkit/vless.py
@@ -1,8 +1,7 @@
 import re
-
 #vless://7de1b798-fa3e-4029-b3f6-62959c1d6c0b@ngiandzang.nguyendinhnghi.id.vn:443?type=tcp&security=tls&fp=&alpn=h2%2Chttp%2F1.1%2Ch3&allowInsecure=1&sni=dl.kgvn.garenanow.com#3%09%09%09%F0%9F%8D%80Nguy%E1%BB%85n%20Ngi%20And%20Nguy%E1%BB%85n%20Zang%F0%9F%92%96%20Vless%20Tls%20Hyper%20NetWork%E2%84%A2%EF%B8%8F-owngxk57
 
-def edit(link, set_uuid=None, set_sni=None, set_tag=None):
+def edit(link, set_uuid, set_sni, set_tag):
   link = link.split('://')[1]
   uuid = link.split('@')[0]
   ip, port = link.split('@')[1].split('?')[0].split(':')
@@ -10,7 +9,7 @@ def edit(link, set_uuid=None, set_sni=None, set_tag=None):
     sni = link.split('sni=')[1].split('#')[0]
     tag = link.split('#')[-1]
   else:
-    sni = re.search(r'host=([^&]+)&.*#(.+)', link).group(1)
+    sni = link.split('host=')[1].split('&')[0]
     tag = re.search(r'#(.+)', link).group(1)
   if ip in ['127.0.0.1', '1.1.1.1', '0.0.0.0', '8.8.8.8']:
     return
