6424a0d63a77ff4d28ea6a4faade7a08896e01d6
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-04 09:15:50 +0700

    update configkit/vless.py

--- configkit/vless.py
+++ configkit/vless.py
@@ -1,22 +1,23 @@
-import re
-#vless://7de1b798-fa3e-4029-b3f6-62959c1d6c0b@ngiandzang.nguyendinhnghi.id.vn:443?type=tcp&security=tls&fp=&alpn=h2%2Chttp%2F1.1%2Ch3&allowInsecure=1&sni=dl.kgvn.garenanow.com#3%09%09%09%F0%9F%8D%80Nguy%E1%BB%85n%20Ngi%20And%20Nguy%E1%BB%85n%20Zang%F0%9F%92%96%20Vless%20Tls%20Hyper%20NetWork%E2%84%A2%EF%B8%8F-owngxk57
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 
 def edit(link, set_uuid, set_sni, set_tag):
-  link = link.split('://')[1]
-  uuid = link.split('@')[0]
-  ip, port = link.split('@')[1].split('?')[0].split(':')
-  tag = link.split('#')[-1]
-  if port == 443:
-    sni = link.split('sni=')[1].split('#')[0]
-  else:
-    sni = link.split('host=')[1].split('&')[0]
+  link = urlparse(link)
+  query = parse_qs(link.query)
+  netloc = link.netloc.split('@')
+  ip = netloc[1].split(':')[0]
   if ip in ['127.0.0.1', '1.1.1.1', '0.0.0.0', '8.8.8.8']:
     return
   if set_uuid:
-    link = link.replace(uuid, set_uuid)
+    netloc[0] = set_uuid
+    netloc = "@".join(netloc)
+    link = link._replace(netloc=netloc)
   if set_sni:
-    link = link.replace(sni, set_sni)
+    if query['security'] == 'tls' and query['type'] == 'tcp':
+      query['sni'] = [set_sni]
+    else:
+      query['host'] = [set_sni]
+    query = urlencode(query, doseq=True)
   if set_tag:
-    link = link.replace(tag, set_tag)
-  full_link = f"vless://{link}"
-  return full_link
+    link = link._replace(fragment=set_tag)
+  link = urlunparse(link)
+  return link
