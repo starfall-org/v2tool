b230edee2e18ee83e3230ab3f31464b8c5f9dd8a
chantroi <tonglanhchua@gmail.com>
2024-02-05 15:36:06 +0700

    update data.py

--- data.py
+++ data.py
@@ -1,13 +1,20 @@
 from deta import Deta
 import os
 
-proxies = dict(http="http://127.0.0.1:8888", https="http:127.0.0.1:8888")
 deta = Deta(os.getenv('DETA_KEY'))
 db = deta.Base("notes")
 
+class Proxy:
+    @staticmethod
+    def set(config):
+        db.put(key="proxy", data=config)
+        return "DONE"
+    def get():
+        
+
 def get_data(filename):
-  entry = db.get(filename)
-  if entry:
-    return entry['urls']
-  else:
-    raise Exception("Không tìm thấy dữ liệu")
+    entry = db.get(filename)
+    if entry:
+        return entry['urls']
+    else:
+        raise Exception("Không tìm thấy dữ liệu")
