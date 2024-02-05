da883d2c01216c93aad1335f9e3246362b1d9530
chantroi <tonglanhchua@gmail.com>
2024-02-05 18:03:26 +0700

    update data.py

--- data.py
+++ data.py
@@ -7,6 +7,7 @@ import time
 deta = Deta(os.getenv('DETA_KEY'))
 db = deta.Base("notes")
 proxy = "http://127.0.0.1:11288"
+expath = os.path.abspath(__file__).replace("/data.py", "")
 
 def test_proxy():
     start_time = time.time()
@@ -26,7 +27,7 @@ class Proxy:
     @staticmethod
     def add(config):
         db.put(key="proxy", data=config)
-        os.system(f"./lite -p 11288 {config} &")
+        os.system(f"{expath}/lite -p 11288 {config} &")
         os.environ["http_proxy"]=proxy
         os.environ["https_proxy"]=proxy
         return test_proxy()
@@ -34,7 +35,7 @@ class Proxy:
     @staticmethod
     def run():
         config = db.get("proxy")["value"]
-        os.system(f"./lite -p 11288 {config} &")
+        os.system(f"{expath}/lite -p 11288 {config} &")
         os.environ["http_proxy"]=proxy
         os.environ["https_proxy"]=proxy
         return test_proxy()
