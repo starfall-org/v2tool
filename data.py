1657ef51769fd84c703be8e1fdcbd17f7eefeadc
chantroi <tonglanhchua@gmail.com>
2024-02-05 15:44:21 +0700

    update data.py

--- data.py
+++ data.py
@@ -8,9 +8,16 @@ class Proxy:
     @staticmethod
     def set(config):
         db.put(key="proxy", data=config)
-        return "DONE"
-    def get():
+        return "OK"
         
+    @staticmethod
+    def get():
+        config = db.get("proxy")["value"]
+        proxy = "http://127.0.0.1:8888"
+        os.system(f"./lite -p 8888 {config}")
+        os.environ["http_proxy"]=proxy
+        os.environ["https_proxy"]=proxy
+        return "OK"
 
 def get_data(filename):
     entry = db.get(filename)
