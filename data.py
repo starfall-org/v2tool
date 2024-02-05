0298aa3e1ebe5a4789a93f247f02aa151367ad20
chantroi <tonglanhchua@gmail.com>
2024-02-05 16:19:20 +0700

    update data.py

--- data.py
+++ data.py
@@ -1,14 +1,28 @@
 from deta import Deta
+from threading import Thread
 import os
 
-workers = os.getenv('WORKERS')
-proxy = os.getenv('PROXY')
 deta = Deta(os.getenv('DETA_KEY'))
 db = deta.Base("notes")
 
+class Proxy:
+    @staticmethod
+    def add(config):
+        db.put(key="proxy", data=config)
+        return "OK"
+        
+    @staticmethod
+    def run():
+        config = db.get("proxy")["value"]
+        proxy = "http://127.0.0.1:8888"
+        os.system(f"./lite -p 8888 {config} &")
+        os.environ["http_proxy"]=proxy
+        os.environ["https_proxy"]=proxy
+        return "OK"
+
 def get_data(filename):
-  entry = db.get(filename)
-  if entry:
-    return entry['urls']
-  else:
-    raise Exception("Không tìm thấy dữ liệu")
+    entry = db.get(filename)
+    if entry:
+        return entry['urls']
+    else:
+        raise Exception("Không tìm thấy dữ liệu")
