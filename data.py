986111e653fe259e5b69eaffb2ceb8b0ac1cf2e3
chantroi <tonglanhchua@gmail.com>
2024-02-05 17:34:21 +0700

    update data.py

--- data.py
+++ data.py
@@ -2,45 +2,39 @@ from deta import Deta
 from threading import Thread
 import requests
 import os
+import time
 
 deta = Deta(os.getenv('DETA_KEY'))
 db = deta.Base("notes")
-proxy = "http://127.0.0.1:8888"
+proxy = "http://127.0.0.1:11288"
+
+def test_proxy():
+    start_time = time.time()
+    while True:
+        r = requests.get("https://www.google.com/generate_204")
+        if r.status_code == 204:
+            return True
+        if time.time() - start_time >= 3:
+            del os.environ["http_proxy"]
+            del os.environ["http_proxy"]
+            return False
 
 class Proxy:
     @staticmethod
     def add(config):
         db.put(key="proxy", data=config)
-        os.system(f"./lite -p 8888 {config} &")
+        os.system(f"./lite -p 11288 {config} &")
         os.environ["http_proxy"]=proxy
         os.environ["https_proxy"]=proxy
-        try:
-            r = requests.get("https://www.google.com/generate_204")
-            if r.status_code != 204:
-                raise
-        except Exception as e:
-            print(e)
-            del os.environ["http_proxy"]
-            del os.environ["https_proxy"]
-            return False
-        return True
+        return test_proxy()
         
     @staticmethod
     def run():
         config = db.get("proxy")["value"]
-        os.system(f"./lite -p 8888 {config} &")
+        os.system(f"./lite -p 11288 {config} &")
         os.environ["http_proxy"]=proxy
         os.environ["https_proxy"]=proxy
-        try:
-            r = requests.get("https://www.google.com/generate_204")
-            if r.status_code != 204:
-                raise
-        except Exception as e:
-            print(e)
-            del os.environ["http_proxy"]
-            del os.environ["https_proxy"]
-            return False
-        return True
+        return test_proxy()
 
 def get_data(filename):
     entry = db.get(filename)
