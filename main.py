ffa9cfb16fd222fb1b26d38a2c8ebc0b32a837e1
chantroi <tonglanhchua@gmail.com>
2024-02-05 16:32:11 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,76 +1,52 @@
 from .http_req import get_response, get_responses
 from .editor import processes
-from .data import get_data
+from .data import get_data, Proxy
 from urllib.parse import unquote
 import base64
 
 def main(context):
-  if context.req.path == "/":
-    return context.res.send(process_query(context))
-  elif context.req.path.startswith("/list/"):
-    return context.res.send(get_all_urls(context))
-  elif "get" in context.req.path:
-    return context.res.send(process_all_config(context))
-  else:
-    return context.res.send("Đường dẫn không hợp lệ.")
+    if not context.req.path.startswith("/proxy"):
+        Proxy.run()
+    if context.req.path == "/":
+        return context.res.send(process_query(context))
+    elif context.req.path.startswith("/get"):
+        return context.res.send(process_all_config(context))
+    elif context.req.path.startswith("/proxy"):
+        config = context.req.query.get("add")
+        if config:
+            Proxy.add(config)
+            return context.res.send("Thiết lập hoàn tất")
+        else:
+            return context.res.send("Vui lòng cung cấp link proxy")
+    else:
+        return context.res.send("Đường dẫn không hợp lệ.")
         
 def process_query(context):
+        query_url = context.req.query.get('url')
+        if not query_url:
+            return "Vui lòng cung cấp tham số URL"
+        uuid = context.req.query.get('uuid')
+        sni = context.req.query.get('sni')
+        tag = context.req.query.get('tag')
+        query_url = unquote(query_url)
+        list_links = get_response(query_url)
+        links = processes(list_links, uuid, sni, tag)
+        links = '\n'.join(links).encode('utf-8')
+        result = base64.b64encode(links).decode('utf-8')
+        return result
+      
+def process_all_config(context):
+    req_path = context.req.path
+    filename = req_path.replace("/get/", "")
+    uuid = context.req.query.get('uuid')
+    sni = context.req.query.get('sni')
+    tag = context.req.query.get('tag')
     try:
-      query_url = context.req.query['url']
-    except Exception:
-      query_url = None
-    if not query_url:
-        return "Vui lòng cung cấp tham số URL"
-    try:
-      uuid = context.req.query['uuid']
-    except Exception:
-      uuid = None
-    try:
-      sni = context.req.query['sni']
-    except Exception:
-      sni = None
-    try:
-      tag = context.req.query['tag']
-    except Exception:
-      tag = None
-    query_url = unquote(query_url)
-    list_links = get_response(query_url)
+        urls = get_data(filename)
+    except Exception as e:
+        return str(e)
+    list_links = get_responses(urls)
     links = processes(list_links, uuid, sni, tag)
     links = '\n'.join(links).encode('utf-8')
     result = base64.b64encode(links).decode('utf-8')
     return result
-    
-def get_all_urls(context):
-    try:
-      req_path = context.req.path
-      filename = req_path.replace("/list/", "")
-      urls = get_data(filename)
-      resp = '\n'.join(urls)
-      return resp
-    except Exception:
-      return "Kho lưu trữ không tồn tại"
-      
-def process_all_config(context):
-  req_path = context.req.path
-  filename = req_path.replace("/get/", "")
-  try:
-    uuid = context.req.query['uuid']
-  except Exception:
-    uuid = None
-  try:
-    sni = context.req.query['sni']
-  except Exception:
-    sni = None
-  try:
-    tag = context.req.query['tag']
-  except Exception:
-    tag = None
-  try:
-    urls = get_data(filename)
-  except Exception as e:
-    return str(e)
-  list_links = get_responses(urls)
-  links = processes(list_links, uuid, sni, tag)
-  links = '\n'.join(links).encode('utf-8')
-  result = base64.b64encode(links).decode('utf-8')
-  return result
