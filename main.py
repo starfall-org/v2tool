095493f838b77f704504bcb85c5f8eb236836a29
chantroi <tonglanhchua@gmail.com>
2024-02-05 15:54:42 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, Response
-from http_req import get_response, get_responses, get_responses_async
+from http_req import get_response, get_responses
 from editor import processes
-from data import get_data
+from data import get_data, Proxy
 from urllib.parse import unquote
 import base64
 import asyncio
@@ -15,11 +15,10 @@ asyncio.set_event_loop(loop)
 def process_query():
     query_url = request.args.get('url')
     if not query_url:
-      return "Vui lòng cung cấp tham số URL", 200
+        return "Vui lòng cung cấp tham số URL", 200
     uuid = request.args.get('uuid')
     sni = request.args.get('sni')
     tag = request.args.get('tag')
-    headers = {"User-Agent": "v2rayNG/1.8.12"}
     query_url = unquote(query_url)
     list_links = get_response(query_url)
     links = processes(list_links, uuid, sni, tag)
@@ -30,39 +29,22 @@ def process_query():
 @app.route('/list/<filename>')
 def get_all_urls(filename):
     try:
-      urls = get_data(filename)
-      resp = make_response('\n'.join(urls))
-      resp.mimetype = 'text/plain'
-      return resp
+        urls = get_data(filename)
+        return Response('\n'.join(urls), mimetype='text/plain')
     except Exception as e:
-      return {"status": "failed", "message": str(e)}, 404
+        return {"status": "failed", "message": str(e)}, 404
      
 @app.route('/get/<filename>')
 def process_all_config(filename):
-  uuid = request.args.get('uuid')
-  sni = request.args.get('sni')
-  tag = request.args.get('tag')
-  try:
-    urls = get_data(filename)
-  except Exception as e:
-    return {"status": "failed", "message": str(e)}, 404
-  list_links = get_responses(urls)
-  links = processes(list_links, uuid, sni, tag)
-  links = '\n'.join(links).encode('utf-8')
-  result = base64.b64encode(links).decode('utf-8')
-  return result #Response(result, mimetype='text/plain')
-  
-@app.route('/getv2/<filename>')
-def process_all_config_async(filename):
-  uuid = request.args.get('uuid')
-  sni = request.args.get('sni')
-  tag = request.args.get('tag')
-  try:
-    urls = get_data(filename)
-  except Exception as e:
-    return {"status": "failed", "message": str(e)}, 404
-  list_links = loop.run_until_complete(get_responses_async(urls))
-  links = processes(list_links, uuid, sni, tag)
-  links = '\n'.join(links).encode('utf-8')
-  result = base64.b64encode(links).decode('utf-8')
-  return result #Response(result, mimetype='text/plain')
+    uuid = request.args.get('uuid')
+    sni = request.args.get('sni')
+    tag = request.args.get('tag')
+    try:
+        urls = get_data(filename)
+    except Exception as e:
+        return {"status": "failed", "message": str(e)}, 404
+    list_links = get_responses(urls)
+    links = processes(list_links, uuid, sni, tag)
+    links = '\n'.join(links).encode('utf-8')
+    result = base64.b64encode(links).decode('utf-8')
+    return Response(result, mimetype='text/plain')
