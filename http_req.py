d87e0516c1ae5b6021fc7be35929012508201423
chantroi <tonglanhchua@gmail.com>
2024-02-05 15:37:41 +0700

    update http_req.py

--- http_req.py
+++ http_req.py
@@ -9,33 +9,34 @@ def get_response(url):
     response = requests.get(url, timeout=3, headers={"User-Agent": "v2rayNG/*.*.*"})
     links = []
     if any(proto in response for proto in ["vmess:", "trojan:", "vless:"]):
-      for link in response.splitlines():
-        if any(proto in link for proto in ["vmess:", "trojan:", "vless:"]):
-          links.append(link)
+        for link in response.splitlines():
+            if any(proto in link for proto in ["vmess:", "trojan:", "vless:"]):
+                links.append(link)
     elif any(proto in response for proto in ["http:", "https:"]):
-      url_pattern = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
-      sub_urls = re.findall(url_pattern, response)
-      links = get_responses(sub_urls)
+        url_pattern = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+        sub_urls = re.findall(url_pattern, response)
+        links = get_responses(sub_urls)
     else:
         decoded_line = base64.b64decode(response).decode('utf-8')
         for link in decoded_line.splitlines():
-          if any(proto in link for proto in ["vmess:", "trojan:", "vless:"]):
-            links.append(link)
+            if any(proto in link for proto in ["vmess:", "trojan:", "vless:"]):
+                links.append(link)
     return links
     
 def get_responses(urls):
-  links = []
-  def process(url):
-    sub_response = requests.get(url, timeout=3, headers={"User-Agent": "v2rayNG/1.8.12"})
-    if any(proto in sub_response for proto in ["vmess:", "trojan:", "vless:"]):
-      links.extend(sub_response.splitlines())
-    else:
-      try:
-        decoded_line = base64.b64decode(sub_response).decode('utf-8')
-        if any(proto in decoded_line for proto in ["vmess:", "trojan:", "vless:"]):
-          links.extend(decoded_line.splitlines())
-      except Exception as e:
-        print(e)
-  with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
-    executor.map(process, urls)
-  return links
+    links = []
+    def process(url):
+        sub_response = requests.get(url, timeout=3, headers={"User-Agent": "v2rayNG/1.8.12"})
+        if any(proto in sub_response for proto in ["vmess:", "trojan:", "vless:"]):
+            links.extend(sub_response.splitlines())
+        else:
+            try:
+                decoded_line = base64.b64decode(sub_response).decode('utf-8')
+                if any(proto in decoded_line for proto in ["vmess:", "trojan:", "vless:"]):
+                    links.extend(decoded_line.splitlines())
+            except Exception as e:
+                print(e)
+                
+    with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+        executor.map(process, urls)
+    return links
