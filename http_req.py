1617e170ac3b2604a00dd4c914c41eecf6886f2e
chantroi <tonglanhchua@gmail.com>
2024-02-05 15:21:48 +0700

    update http_req.py

--- http_req.py
+++ http_req.py
@@ -2,10 +2,8 @@ import base64
 import json
 import requests
 import re
-import aiohttp
-import asyncio
 import concurrent.futures
-from data import workers, proxy
+from data import workers, proxies
 
 def get_response(url):
     try:
@@ -13,11 +11,9 @@ def get_response(url):
         if response.status_code != 200:
             raise
         response = response.text
-    except:
-        try:
-            response = requests.get(proxy, params={"url": url}, timeout=3).text
-        except:
-            response = requests.get(workers, params={"url": url}, timeout=3).text
+    except Exception as e:
+        print(e)
+        response = requests.get(url, timeout=3, headers={"User-Agent": "v2rayNG/1.8.12"}, proxies=proxies)
     links = []
     if any(proto in response for proto in ["vmess:", "trojan:", "vless:"]):
       for link in response.splitlines():
@@ -42,11 +38,9 @@ def get_responses(urls):
       if sub_response.status_code != 200:
         raise
       sub_response = sub_response.text
-    except:
-      try:
-          sub_response = requests.get(proxy, params={"url": url}, timeout=3).text
-      except:
-          sub_response = requests.get(workers, params={"url": url}, timeout=3).text
+    except Exception as e:
+        print(e)
+        sub_response = requests.get(url, timeout=3, headers={"User-Agent": "v2rayNG/1.8.12"}, proxies=proxies)
     if any(proto in sub_response for proto in ["vmess:", "trojan:", "vless:"]):
       links.extend(sub_response.splitlines())
     else:
@@ -54,39 +48,40 @@ def get_responses(urls):
         decoded_line = base64.b64decode(sub_response).decode('utf-8')
         if any(proto in decoded_line for proto in ["vmess:", "trojan:", "vless:"]):
           links.extend(decoded_line.splitlines())
-      except:
-        pass
+      except Exception as e:
+        print(e)
   with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
     executor.map(process, urls)
   return links
 
-async def async_process(session, url, proxy, workers, links):
-    try:
-        async with session.get(url, timeout=3, headers={"User-Agent": "v2rayNG/1.8.12"}) as sub_response:
-            if sub_response.status != 200:
-                raise Exception("Request failed")
-            sub_response = await sub_response.text()
-    except:
-        try:
-            sub_response = await session.get(proxy, params={"url": url}, timeout=3)
-            sub_response = await sub_response.text()
-        except:
-            sub_response = await session.get(workers, params={"url": url}, timeout=3)
-            sub_response = await sub_response.text()
-
-    if any(proto in sub_response for proto in ["vmess:", "trojan:", "vless:"]):
-        links.extend(sub_response.splitlines())
-    else:
-        try:
-            decoded_line = base64.b64decode(sub_response).decode('utf-8')
-            if any(proto in decoded_line for proto in ["vmess:", "trojan:", "vless:"]):
-                links.extend(decoded_line.splitlines())
-        except:
-            pass
-
-async def get_responses_async(urls):
-    links = []
-    async with aiohttp.ClientSession() as session:
-        tasks = [async_process(session, url, proxy, workers, links) for url in urls]
-        await asyncio.gather(*tasks)
-    return links
+# async def async_process(session, url, proxy, workers, links):
+#     try:
+#         async with session.get(url, timeout=3, headers={"User-Agent": "v2rayNG/1.8.12"}) as sub_response:
+#             if sub_response.status != 200:
+#                 raise Exception("Request failed")
+#             sub_response = await sub_response.text()
+#     except Exception as e:
+#         print(e)
+#         try:
+#             sub_response = await session.get(proxy, params={"url": url}, timeout=3)
+#             sub_response = await sub_response.text()
+#         except Exception as e:
+#             sub_response = await session.get(workers, params={"url": url}, timeout=3)
+#             sub_response = await sub_response.text()
+# 
+#     if any(proto in sub_response for proto in ["vmess:", "trojan:", "vless:"]):
+#         links.extend(sub_response.splitlines())
+#     else:
+#         try:
+#             decoded_line = base64.b64decode(sub_response).decode('utf-8')
+#             if any(proto in decoded_line for proto in ["vmess:", "trojan:", "vless:"]):
+#                 links.extend(decoded_line.splitlines())
+#         except Exception as e:
+#             print(e)
+# 
+# async def get_responses_async(urls):
+#     links = []
+#     async with aiohttp.ClientSession() as session:
+#         tasks = [async_process(session, url, proxy, workers, links) for url in urls]
+#         await asyncio.gather(*tasks)
+#     return links
