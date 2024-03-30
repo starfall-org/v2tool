d6dd6647f036759ac79aaed63808320510cd0c38
chantroi <tonglanhchua@gmail.com>
2024-03-30 18:08:32 +0700

    update editor.py

--- editor.py
+++ editor.py
@@ -10,11 +10,26 @@ def editor(
     ):
   for link in batch:
     if link.startswith('vmess'):
-      link = vmess.edit(link, uuid, sni, tag)
+      link = vmess.edit(
+          link, 
+          uuid,
+          sni,
+          tag
+          )
     elif link.startswith('trojan'):
-      link = trojan.edit(link, uuid, sni, tag)
+      link = trojan.edit(
+          link, 
+          uuid, 
+          sni,
+          tag
+          )
     elif link.startswith('vless'):
-      link = vless.edit(link, uuid, sni, tag)
+      link = vless.edit(
+          link,
+          uuid,
+          sni,
+          tag
+          )
     if link:
       values.add(link)
   
@@ -27,15 +42,29 @@ def editor(
 #     values.extend(value)
 #   return values
   
-def processes(links, uuid=None, sni=None, tag=None):
+def processes(
+    links,
+    uuid=None,
+    sni=None,
+    tag=None
+    ):
   batch_size = 10
   values = set()
   def process_batch(batch):
     try:
-        editor(batch, values, uuid, sni, tag)
+        editor(
+            batch,
+            values,
+            uuid,
+            sni,
+            tag
+            )
     except Exception as e:
         print(e)
         pass
   with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(process_batch, (links[i:i + batch_size] for i in range(0, len(links), batch_size)))
+    executor.map(
+        process_batch,
+        (links[i:i + batch_size] for i in range(0, len(links), batch_size))
+            )
   return list(values)
