712c30e03be24fb04802d295c3dddaf4373710f2
chantroi <tonglanhchua@gmail.com>
2024-03-30 11:28:36 +0700

    update data.py

--- data.py
+++ data.py
@@ -7,16 +7,18 @@ import time
 deta = Deta(os.getenv('DETA_KEY'))
 db = deta.Base("notes")
 proxy = "http://127.0.0.1:10808"#.format(os.getenv('PROXY'))
+proxies = {
+    "http": proxy,
+    "https": proxy
+    }
 
 def test_proxy():
     start_time = time.time()
     while True:
         if time.time() - start_time >= 3:
-            del os.environ["http_proxy"]
-            del os.environ["https_proxy"]
             return False
         try:
-            requests.get("https://www.google.com/generate_204", timeout=1)
+            requests.get("https://www.google.com/generate_204", timeout=1, proxies=proxies)
             return True
         except Exception:
             continue
@@ -26,16 +28,12 @@ class Proxy:
     def add(config):
         db.put(key="proxy", data=config)
         os.system(f"./lite -p 10808 {config} &")
-        os.environ["http_proxy"]=proxy
-        os.environ["https_proxy"]=proxy
         return test_proxy()
         
     @staticmethod
     def run():
         config = db.get("proxy")["value"]
         os.system(f"./lite -p 10808 {config} &")
-        os.environ["http_proxy"]=proxy
-        os.environ["https_proxy"]=proxy
         return test_proxy()
 
 def get_data(filename):
