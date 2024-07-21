9042b7ce506ec847667f4c9615e47ba5845b32cd
chantroi <tonglanhchua@gmail.com>
2024-07-21 07:42:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import os
 from src.application import app
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
+    app.run(port=os.getenv("PORT", default=5000), host='0.0.0.0')
