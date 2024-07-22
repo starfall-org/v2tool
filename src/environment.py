475c80dd5c589bdd5e3763a70d027e71e9f7ec8c
AzureVM <tonglanhchua@gmail.com>
2024-07-23 06:44:26 +0700

    update environment.py

--- src/environment.py
+++ src/environment.py
@@ -4,5 +4,5 @@ import requests
 keys_url = os.environ["SECRET"]
 req = requests.get(keys_url, timeout=99)
 response = req.json()
-db_url = response["sql"]["turso"]
-writer = response["bot"]["writer"]
+db_url = response["db"]["libsql"]
+writer = response["access"]["telegram"]["writer"]
