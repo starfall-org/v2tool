846da3afedc0beb7b0af4f18ec2a728a8775677b
Chan Troi Moi <tonglanhchua@gmail.com>
2024-08-07 13:52:26 +0000

    update editor.py

--- src/editor.py
+++ src/editor.py
@@ -1,27 +1,20 @@
 from .protocols import trojan, vless, vmess
 
 
-def editor(
-    links: list, values: set, uuid: str = None, sni: str = None, tag: str = None
-):
+def processes(links: list, uuid: str = None, sni: str = None, tag: str = None):
+    duplicate = []
+    results = []
     for link in links:
         if link.startswith("vmess"):
-            link = vmess.edit(link, uuid, sni, tag)
+            result = vmess.edit(link, uuid, sni, tag)
         elif link.startswith("trojan"):
-            link = trojan.edit(link, uuid, sni, tag)
+            result = trojan.edit(link, uuid, sni, tag)
         elif link.startswith("vless"):
-            link = vless.edit(link, uuid, sni, tag)
-        if link:
-            values.add(link)
-
-
-def processes(links: list, uuid: str = None, sni: str = None, tag: str = None):
-    values = set()
-    duplicate = []
-    result = []
-    editor(links, values, uuid, sni, tag)
-    for value in values:
-        if value[1] not in duplicate:
-            result.append(value[0])
-            duplicate.append(value[1])
-    return result
+            result = vless.edit(link, uuid, sni, tag)
+        else:
+            result = link
+        if result and len(result) == 2:
+            if result[1] not in duplicate:
+                results.append(result[0])
+                duplicate.append(result[1])
+    return results
