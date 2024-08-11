4d67ac377d46f4353e4565cca92205937c818727
Chan Troi Moi <tonglanhchua@gmail.com>
2024-08-11 12:46:41 +0000

    update editor.py

--- src/editor.py
+++ src/editor.py
@@ -5,16 +5,16 @@ def processes(links: list, uuid: str = None, sni: str = None, tag: str = None):
     duplicate = []
     results = []
     for link in links:
-        if link.startswith("vmess"):
+        result = None
+        if link.startswith("vmess://"):
             result = vmess.edit(link, uuid, sni, tag)
-        elif link.startswith("trojan"):
+        elif link.startswith("trojan://"):
             result = trojan.edit(link, uuid, sni, tag)
-        elif link.startswith("vless"):
+        elif link.startswith("vless://"):
             result = vless.edit(link, uuid, sni, tag)
         else:
             results.append(link)
-        if result and isinstance(result, tuple) and len(result) == 2:
-            if result[1] not in duplicate:
-                results.append(result[0])
-                duplicate.append(result[1])
+        if result and result[1] not in duplicate:
+            results.append(result[0])
+            duplicate.append(result[1])
     return results
