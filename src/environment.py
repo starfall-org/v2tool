0141579ea54cad3ad5f6572f5344d30c3f993ab2
Chan Troi Moi <tonglanhchua@gmail.com>
2024-09-22 00:38:26 +0000

    update environment.py

--- src/environment.py
+++ src/environment.py
@@ -4,5 +4,5 @@ import requests
 keys_url = os.environ["SECRET"]
 req = requests.get(keys_url, timeout=99)
 response = req.json()
-db_url = response["db"]["libsql"]
-writer = response["access"]["telegram"]["writer"]
+db_url = response["database"]["libsql"]
+writer = response["telegram"]["bot"]["writer"]
