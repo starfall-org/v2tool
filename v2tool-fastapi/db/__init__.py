fc1b2daae63756867f84dc81205c8cc05fc4af22
chantroi <tonglanhchua@gmail.com>
2025-01-15 17:12:02 +0700

    ....

--- v2tool-fastapi/db/__init__.py
+++
@@ -1,4 +0,0 @@
-from .client import engine
-from .models import *
-
-Base.metadata.create_all(bind=engine)
--- v2tool-fastapi/db/client.py
+++
@@ -1,7 +0,0 @@
-import os
-from sqlalchemy import create_engine
-
-db_url: str = os.getenv("DATABASE_URL")
-engine: object = create_engine(
-    db_url, connect_args={"check_same_thread": False}, echo=True, pool_pre_ping=True
-)
